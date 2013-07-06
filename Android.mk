b959d47193c3383a36822d37bf7e1d957cc2e6da
ivan198 <ivan198@gmail.com>
2013-07-06 16:56:10 +0800

    Hack for build

--- Android.mk
+++ Android.mk
@@ -1,3 +1,5 @@
+# Hack for build 
+$(shell mkdir -p $(OUT)/obj/KERNEL_OBJ/usr/include)
 LOCAL_PATH := $(call my-dir)
 ifeq ($(TARGET_BOOTLOADER_BOARD_NAME),c8813)
 include $(call all-makefiles-under,$(LOCAL_PATH))
